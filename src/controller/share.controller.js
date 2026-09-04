f1ac315dd19689b189fdb2ec991ad7963570a6be
Ruchi Jian <learnwithfunroo@gmail.com>
2026-09-04 17:20:17 +0530

    made small changes

--- src/controller/share.controller.js
+++ src/controller/share.controller.js
@@ -5,11 +5,14 @@ import { createShareModel, getShareModel, deleteShareModel, getSharedWithMeModel
 import { findUserByEmail } from "../model/user.model.js";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    requireTLS: true,
 });
 
 export const createShare = async (req, res) => {
